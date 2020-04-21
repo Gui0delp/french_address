2403c99eab9f43ab78d0349d0d858fe1e2a25505
Gui0delp <delpro.guillaume@gmail.com>
2020-04-21 13:18:42 +0200

    Minor modification

--- modules/point_tool.py
+++ modules/point_tool.py
@@ -1,6 +1,7 @@
 from qgis.gui import QgsMapTool
 
 from .coordinates import Coordinates
+from .message_handler import MessageHandler
 class PointTool(QgsMapTool):
     """Point tool"""
 
@@ -9,6 +10,7 @@ class PointTool(QgsMapTool):
         self.canvas = canvas
         self.dialog = dialog
         self.coord = Coordinates(self.dialog)
+        self.message_handler = MessageHandler(self.dialog)
 
     def canvasReleaseEvent(self, event):
         """Get the clic from the mouss"""
@@ -22,3 +24,5 @@ class PointTool(QgsMapTool):
             point = self.canvas.getCoordinateTransform().toMapCoordinates(x, y)
             self.coord.set_point_to_wgs84(point)
             self.coord.set_latitude_longitude_wgs84()
+            message = "{} | {}".format(str(self.coord.latitude), str(self.coord.longitude))
+            self.message_handler.send_logs_messages('ok', message)
